7e8cc275654b5bbc1de392602966b68930b7299e
Reisorz <miguelminib@gmail.com>
2025-04-02 17:27:26 +0200

    ADD MainLayout and navbar.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,20 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './features/login/login.component';
 import { RegisterComponent } from './features/register/register.component';
 import { SearchMatchComponent } from './features/search-match/search-match/search-match.component';
+import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 
 export const routes: Routes = [
-    {path:'login', component: LoginComponent},
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'register', component: RegisterComponent},
-    {path: 'search-match', component: SearchMatchComponent}
-];
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    
+    //Routes with navbar and footer 
+    {
+      path: '',
+      component: MainLayoutComponent,
+      children: [
+        { path: 'search-match', component: SearchMatchComponent }
+      ]
+    }
+  ];
