30d138396ff92a21a23adb59668de011d5f3b83d
Reisorz <miguelminib@gmail.com>
2025-04-14 16:01:51 +0200

    FIX detect browser to allow localStorage to work.

--- src/app/core/service/token.service.ts
+++ src/app/core/service/token.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 import { jwtDecode } from "jwt-decode";
 
 @Injectable({
@@ -7,28 +8,37 @@ import { jwtDecode } from "jwt-decode";
 export class TokenService {
 
   private TOKEN_KEY = "accessToken";
+  private isBrowser: boolean;
 
-  constructor() { }
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
 
-  public setAccessToken(token: string) {
-    localStorage.setItem(this.TOKEN_KEY, token);
+  public setAccessToken(token: string): void {
+    if (this.isBrowser) {
+      localStorage.setItem(this.TOKEN_KEY, token);
+    }
   }
 
   public getAccessToken(): string | null {
-    return localStorage.getItem(this.TOKEN_KEY);
+    if (this.isBrowser) {
+      return localStorage.getItem(this.TOKEN_KEY);
+    }
+    return null;
   }
 
-  public removeAccessToken() {
-    localStorage.removeItem(this.TOKEN_KEY);
+  public removeAccessToken(): void {
+    if (this.isBrowser) {
+      localStorage.removeItem(this.TOKEN_KEY);
+    }
   }
 
-  public getUserDetailsFromToken() {
+  public getUserDetailsFromToken(): void {
     const token = this.getAccessToken();
-    if(token){
-      const decoded = jwtDecode(token);
+    if (token && this.isBrowser) {
+      const decoded: any = jwtDecode(token);
       const email = decoded.sub ?? '';
       localStorage.setItem("email", email);
     }
   }
-
 }
